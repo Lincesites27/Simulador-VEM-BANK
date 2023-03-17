5ca134e9cb867f8f7b2949618fa4fa4d7928f7b5
Lincesites27 <contato@lincesites.com.br>
2023-03-17 12:51:03 -0300

    arrumei o codigo

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const taxaSessenta = 2;
 const taxaSetenta = 2.02;
 const taxaOitenta = 2.02;
 
-
 const mesDoze = 12;
 const mesVinte = 24;
 const mesTrinta = 36;
@@ -25,15 +24,67 @@ const mesOitenta = 84;
 
 
 
-valorInput.oninput =(()=>{
+valorInput.addEventListener("change",()=>{
     let valueInput = Number(valorInput.value);
-    let valueInputMes = Number(valorInputMes.value);
-    valorTxt.innerHTML = `<h4 id="valorTxt">R$${valueInput.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</h4>`;
-    valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${valueInput.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</h2>`;
+    let divisaoMes = Number(valorInputMes.value);
+    var calculo = Number(valueInput / divisaoMes);
+
+    if(divisaoMes === mesDoze){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaDoze).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesVinte){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaVinteQuatro).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesTrinta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaTrintaSeis).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesQuarenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaQuarentaOito).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesSessenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaSessenta).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesSetenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaSetenta).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesOitenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaOitenta).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
 
 });
 
+valorInput.oninput= (()=>{
+    let valueInput = Number(valorInput.value);
+    valorTxt.innerHTML = `<h4 id="valorTxt">R$${valueInput.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}</h4>`;
+})
+
 valorInputMes.oninput =(()=>{
+    let valueInput = Number(valorInput.value);
+    let divisaoMes = Number(valorInputMes.value);
+    var calculo = Number(valueInput / divisaoMes);
+
+    if(divisaoMes === mesDoze){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaDoze).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesVinte){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaVinteQuatro).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesTrinta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaTrintaSeis).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesQuarenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaQuarentaOito).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesSessenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaSessenta).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesSetenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaSetenta).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+      else if(divisaoMes === mesOitenta){
+        valorEntradaMensalidade.innerHTML = `<h2 id="valorEntradaMensalidade">R$${(calculo*taxaOitenta).toLocaleString('pt-BR', { minimumFractionDigits: 3, maximumFractionDigits: 3 }).replace('.', '.').slice(0, -1)}</h2>`;
+      }
+
     const valorTxtMes = document.querySelector("#valorTxtMes");
     let valueInputMes = Number(valorInputMes.value);
     valorTxtMes.innerHTML = `<h4 id="valorTxtMes">${valueInputMes} <span>Meses</span></h4>`;
